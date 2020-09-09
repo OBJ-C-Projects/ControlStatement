898f53d4b9b7f239d32dae8b84b48d016adb1c34
fritzgt <tuanis@live.com>
2020-09-09 12:20:02 +0300

    Initial commit

---
+++ ControlStatementsC/ControlStatementsC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ControlStatementsC
+//
+//  Created by FGT MAC on 9/9/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
